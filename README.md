b8b2a32d43f1c86f95248dd1e2320fddde5b5419
YaShiho <yaoxh22@gmail.com>
2025-01-02 06:36:18 +0000

    finally

--- README.md
+++ README.md
@@ -19,7 +19,8 @@ bash download_nli.sh
 bash download_wiki.sh
 ```
 注意数据集配置可能出现网络问题，可以在浏览器中下载后将`nli_for_simcse.csv`和`wiki1m_for_simcse.txt`放到`data`目录下
-- 模型导入
+- 模型导入  
+
 将预训练模型导入到`model`文件夹下
 
 ## 训练
@@ -38,3 +39,18 @@ bash run_sup.sh
 ```
 bash evaluate.sh
 ```
+
+## 在使用Jittor时发现的问题
+在使用Jittor的`Dataset`和`DataLoader`类时我们发现了一些问题  
+按照使用Pytorch的习惯，我们会在继承`Dataset`类实现自己的类时实现`__len__`函数类似如下：
+```
+def __len__(self):
+    return len(self.data)
+```
+然而，在使用`DataLoader`封装后，`DataLoader`返回的length与`DataSet`一样，而并没有依据batch size改变
+这是因为`DataLoader`是定义的一个函数而非封装好的一个类，其定义如下：
+```
+def DataLoader(dataset: Dataset, *args, **kargs):
+    return dataset.set_attrs(*args, **kargs)
+```
+这就导致直接使用`tqdm`打印进度条时，进度条长度和实际迭代次数不一致的问题，目前的解决方法是向`tqdm`传入`total`的参数设置正确的进度条长度
--- src/evaluate.sh
+++ src/evaluate.sh
@@ -1,50 +1,11 @@
-# export HF_ENDPOINT=https://hf-mirror.com
-# transformers-cli download bert-base-uncased
-
 python evaluation.py \
-    --model_name_or_path ../output/unsup_p001_t0001.bin \
+    --model_name_or_path ../output/sup_p01_t005_penalty1.bin \
     --task_set sts \
     --mode test \
-    > result_unsup.txt 2>&1 &
-
-# python evaluation.py \
-#     --model_name_or_path ../output/unsup.bin \
-#     --task_set sts \
-#     --mode test
-python evaluation.py \
-    --model_name_or_path ../output/sup_cls.bin \
-    --task_set sts \
-    --mode test \
-    > result.txt 2>&1 &
-
+    > ../results/result_sup_p01_t005_penalty1.txt 2>&1 &
 
 python evaluation.py \
-    --model_name_or_path ../output/unsup_p01_t005_penalty1.bin \
+    --model_name_or_path ../output/unsup_p01_t005.bin \
     --task_set sts \
     --mode test \
-    > result_unsup_p01_t005_penalty1.txt 2>&1 &
-
-
-python evaluation.py \
-    --model_name_or_path ../output/sup_t005_penalty4.bin \
-    --task_set sts \
-    --mode test \
-    > result_sup_t005_penalty4.txt 2>&1 &
-
-python evaluation.py \
-    --model_name_or_path ../output/sup_t005_penalty5.bin \
-    --task_set sts \
-    --mode test \
-    > result_sup_t005_penalty5.txt 2>&1 &
-
-python evaluation.py \
-    --model_name_or_path ../output/sup_t005_penalty6.bin \
-    --task_set sts \
-    --mode test \
-    > result_sup_t005_penalty6.txt 2>&1 &
-
-python evaluation.py \
-    --model_name_or_path ../output/unsup_p01_t005_penalty05_dynamic.bin \
-    --task_set sts \
-    --mode test \
-    > result_unsup_p01_t005_penalty05_dynamic.txt 2>&1 &
+    > ../results/result_unsup_p01_t005.txt 2>&1 &
